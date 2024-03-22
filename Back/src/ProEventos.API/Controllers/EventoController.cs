b0a7c74b10f977dfaa922d153f3a163cb26ce94b
Willian Lima <william_slayer@hotmail.com>
2024-03-22 16:57:57 -0300

    Modificado

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly ILogger<EventoController> _logger;
+
+        public EventoController()
+        {
+           
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+           return "value";
+        }
+    }
+}
