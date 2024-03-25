792ba59b427d9327f50e92e55c02e1d8b5cc8cfd
Willian Lima <william_slayer@hotmail.com>
2024-03-25 11:07:12 -0300

    Criado rotas usando id

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -12,19 +12,50 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        private readonly ILogger<EventoController> _logger;
-
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+                Id = 1,
+                Tema = "Angular + C#",
+                Local = "Belo horizonte",
+                Lote = "1º lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImageURL = "Foto.png"
+                }, 
+                new Evento(){
+                Id = 2,
+                Tema = "Angular + .net",
+                Local = "Salvador",
+                Lote = "2º lote",
+                QtdPessoas = 200,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImageURL = "Foto.png"
+                }     
+        };
         public EventoController()
         {
            
         }
 
         [HttpGet]
-        public Evento Get()
+        public IEnumerable<Evento> Get()
         {
-           return new Evento(){
-            
-           };
+           return _evento;
+        }
+         [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+           return _evento.Where(evento => evento.Id == id);
+        }
+        [HttpPost]
+        public IEnumerable<Evento> Post()
+        {
+           return _evento;
+        }
+        [HttpPut]
+        public IEnumerable<Evento> Put()
+        {
+           return _evento;
         }
     }
 }
