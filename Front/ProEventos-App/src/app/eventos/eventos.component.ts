288ac6261d6a31285045cf6deeba21d72b12233a
Willian Lima <william_slayer@hotmail.com>
2024-04-01 15:42:53 -0300

    Criado app eventos com os componentes eventos e palestratntes.

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.getEventos();
+  }
+
+  getEventos(){
+    this.http.get("https://localhost:5001/api/Eventos").subscribe(
+      res => this.eventos = res,
+      erro => console.log(erro)
+    );
+  }
+}
