9976d965f897d669b195f5efe6422eb963728464
Willian Lima <william_slayer@hotmail.com>
2024-04-04 17:37:09 -0300

    Criado eventos de filtro e busca

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,7 +9,28 @@ import { HttpClient } from '@angular/common/http';
 export class EventosComponent implements OnInit {
 
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrado: any = [];
+
+  exibirImagem: boolean = true;
+  private _filtroLista: string = "";
+
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+
+  public set filtralisra(value: string){
+     this._filtroLista = value;
+     this.eventosFiltrado = this.filtroLista ? this.filtroEvento(this.filtroLista) : this.eventos;
+  }
+
+  filtroEvento(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(
     private http: HttpClient
@@ -19,9 +40,15 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarImagem(){
+    this.exibirImagem = !this.exibirImagem
+  }
+
   getEventos(){
     this.http.get("https://localhost:5001/api/Eventos").subscribe(
-      res => this.eventos = res,      
+      res => {this.eventos = res;
+        this.eventosFiltrado = this.eventos 
+      }      
     )
   }
 
